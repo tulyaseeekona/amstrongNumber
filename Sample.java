83351277a0ed522cffb57b73a77cd42081c5b538
tulyaseeekona <92907794+tulyaseeekona@users.noreply.github.com>
2021-12-22 11:51:45 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ITLab
+ */
+public class Sample {
+
+    public static void main(String args[]) {
+        int n=22;
+        if(n%2 == 0)
+            System.out.println("even no");
+        else
+            System.out.println("not an even no");
+    }
+}
